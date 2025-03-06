86ff4eddb85f967fbab98faf9aa0b6e5401b3456
TaiefArnob <arnob233@gmail.com>
2025-03-06 13:28:46 +0600

    Changed route settings

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance=axios.create({
-    baseURL:"https://bleep-server.onrender.com",
+    baseURL:"https://bleep-server.onrender.com/api",
     withCredentials:true
 })

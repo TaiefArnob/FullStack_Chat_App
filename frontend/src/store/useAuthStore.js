45a3b00cf80e5f1b9420b1dbf13a4d4466ea8796
TaiefArnob <arnob233@gmail.com>
2025-03-07 14:46:44 +0600

    Fixed Signup

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -59,6 +59,21 @@ export const useAuthStore = create((set, get) => ({
     }
   },
 
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully!");
+      get().connectSocket();
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Signup failed");
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+  
+
   login: async (data) => {
     set({ isLoggingIn: true });
     try {
